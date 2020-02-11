8f0db908b32bb0a400d45431065ddd6be4dca72f
ashbob999 <32575256+ashbob999@users.noreply.github.com>
2020-02-11 12:40:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const TOKEN = process.env.TESTBOT;
 
 bot.login(TOKEN);
 
