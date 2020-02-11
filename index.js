a750897575de614cb317edf866c547bb55532b94
ashbob999 <32575256+ashbob999@users.noreply.github.com>
2020-02-11 13:16:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,5 @@ bot.on('message', msg => {
         msg.reply('pong');
         msg.channel.send('pong');
 
-    } else if (msg.content.startsWith('!kick')) {
-        if (msg.mentions.users.size) {
-            const taggedUser = msg.mentions.users.first();
-            msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-        } else {
-            msg.reply('Please tag a valid user!');
-        }
     }
 });
