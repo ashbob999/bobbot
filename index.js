05c6af5498ab981b043c38c32b3ebb9d9c541a98
ashbob999 <ashbob999@gmail.com>
2020-03-03 14:12:19 +0000

    Added Extra Dependencies

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const wakeUpDyno = require("wakeDyno.js");
+
+import { Client } from 'discord.js';
+
+const bot = new Client();
 const TOKEN = process.env.TESTBOT;
+
+const PORT = 3000; // whatever port you like
+const DYNO_URL = ""; // the url of your dyno
+
+const app = express(); // instantiate Express app
+
+app.listen(PORT, () => {
+    wakeUpDyno(DYNO_URL); // will start once server starts
+})
 
 bot.login(TOKEN);
 
