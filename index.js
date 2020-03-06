13243de2dee073f087bfdb563e487b369602d281
ashbob999 <ashbob999@gmail.com>
2020-03-06 14:30:49 +0000

    Removed WakeDyno

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ import { Client } from 'discord.js';
 const bot = new Client();
 const TOKEN = process.env.BOT_TOKEN;
 
-const PORT = 3000; // whatever port you like
-const DYNO_URL = ""; // the url of your dyno
-
-const app = express(); // instantiate Express app
-
-app.listen(PORT, () => {
-    wakeUpDyno(DYNO_URL); // will start once server starts
-})
-
 bot.login(TOKEN);
 
 bot.on('ready', () => {
