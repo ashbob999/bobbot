f97cf9cbcaee1da0876090803f36ddf2b46d5994
ashbob999 <ashbob999@gmail.com>
2020-03-06 14:33:28 +0000

    Changed discord.js to require

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const wakeUpDyno = require("wakeDyno.js");
 
-import { Client } from 'discord.js';
+const Discord = require("discord.js")
 
-const bot = new Client();
+const bot = new Discord.Client();
 const TOKEN = process.env.BOT_TOKEN;
 
 bot.login(TOKEN);
