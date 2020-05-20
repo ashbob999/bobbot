0bbb046978ad3ad5b0ff7ed6fa3c59c4c70b0dc5
ashbob999 <ashbob999@gmail.com>
2020-05-20 14:11:15 +0100

    Added simple help function

--- index.js
+++ index.js
@@ -91,6 +91,25 @@ bot.on('message', msg => {
 	}
 	*/
 	
+	// if help command sent
+	if (args[0] == "help") {
+		// if help has arguments
+		if (args.length > 1) {
+			// send command specific help
+			if (args[1] in commands) {
+				let reply = "Command '" + args[1] + "':\n";
+				reply += "\tDesc: " + commands[args[1]].desc + "\n";
+				reply += "\tUsage: "+ commands[args[1]].usage;
+				msg.channel.send(reply);
+			} else { // invalid command
+				msg.channel.send("'" + args[1] + "' is not a command");
+			}
+			return;
+		} else { // show help for all commands
+			msg.channel.send("help all commands");
+		}
+	}
+
 	if (args[0] in commands) {
 		// if the command has a channel whitelist
 		if (commands[args[0]].whitelist) {
