aa10159cf546455c74a4570db389d8803f304e7f
ashbob999 <ashbob999@gmail.com>
2020-05-27 11:54:54 +0100

    Updated start/stop functions to require a single query.

--- util/Times.js
+++ util/Times.js
@@ -5,19 +5,22 @@ const max_time = 3600; // 1 hour
 
 function start(msg) {
 	// does not update start_time after multiple --start's
-	let query = "INSERT INTO " +
-		"times (user_id, start_time) " +
-		"SELECT '" + msg.author.id +
-		"', '" + db.dateTime() + "' " +
-		"WHERE NOT EXISTS (" + 
-			"SELECT 1 FROM times WHERE " +
-			"user_id = '" + msg.author.id +
-			"' AND time_taken is NULL" +
-		") RETURNING id ;";
+	
+	let query = `
+INSERT INTO times (user_id,
+				   start_time)
+SELECT '${msg.author.id}', '${db.dateTime()}'
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM times
+	WHERE user_id = '${msg.author.id}'
+	AND time_taken is NULL
+);
+`;
 
 	let result = db.getRows(query, (r,e) => {
 		if (!e) {
-			if (r.rows.length) { // insert worked
+			if (r.rowCount > 0) { // insert worked
 				msg.reply("time started.");
 			}
 		}else {
@@ -29,50 +32,52 @@ function start(msg) {
 function stop(msg) {
 	let end_time = db.dateTime();
 
-	let query = "SELECT id, " +
-		"(extract(epoch from " +
-			"to_timestamp('" +
-				end_time + "', " +
-				"'YYYY-MM-DD HH24-MI-SS'))" +
-		" - extract(epoch from start_time)) AS time_diff " +
-		"FROM times " +
-		"WHERE user_id = '" + msg.author.id +
-		"' AND time_taken IS NULL LIMIT 1;";
+	let query = `
+WITH start_row AS
+(
+	SELECT id,
+		(extract(epoch
+			from to_timestamp('${end_time}', 'YYYY-MM-DD HH24-MI-SS'))
+		 - extract(epoch
+			from start_time))
+		AS time_diff
+	FROM times
+	WHERE user_id = '${msg.author.id}'
+		AND time_taken IS NULL LIMIT 1
+)
+, deleted AS
+(
+	DELETE
+	FROM times
+	WHERE id = (
+			SELECT id
+			FROM start_row
+			LIMIT 1)
+		AND (
+			SELECT time_diff
+			FROM start_row
+			LIMIT 1) > 3600
+)
+
+UPDATE times
+SET time_taken = time_diff
+FROM start_row
+WHERE times.id = start_row.id
+	AND time_diff <= 3600
+RETURNING time_taken
+;`;
 
 	let result = db.getRows(query, (r,e) => {
 		if (e) throw e;
 		
-		if (!r.rows.length) {
-			// no rows
-			return;
+		if (r.rowCount > 0) { // success
+			msg.reply("you took " + r.rows[0].time_taken + " seconds");
+		} else {
+			// either:
+			// 1: start hasn't been called
+			// 2: time_taken is > 3600
 		}
-		console.log(r.rows);
-		console.log(end_time);
-		
-		let query2 = "UPDATE times " +
-			"SET time_taken = '" + r.rows[0].time_diff + "' " +
-			"WHERE id = '" + r.rows[0].id + "';";
-
-		db.getRows(query2, (r2, e2) => {
-			if (e2) throw e2;
-
-			console.log("updated");
-			msg.reply("you took " + r.rows[0].time_diff + " seconds");
-		});
 	});
-
-	if (result.error) {
-		throw result.error;
-	}
-
-//console.log(result);
-	/*
-	query = "UPDATE times " +
-		"SET end_time = '" +
-		end_time + "' " +
-		"WHERE id = '" + result.result[0].id + "';";
-*/
-	
 }
 
 function cancel(msg) {
