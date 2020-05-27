6ec2ba1da9ea8ef5bc6473708a059f62a7b9841b
ashbob999 <ashbob999@gmail.com>
2020-05-27 12:04:27 +0100

    Fixed some syntax errors.

--- Commands.js
+++ Commands.js
@@ -24,35 +24,36 @@ let times_whitelist = {
 	"shit-talk": undefined,
 	"bot-channel": undefined,
 	"bot-testing": undefined,
-}
+};
 
 commands.start = {
 	func: util.Times.start,
 	whitelist: times_whitelist,
 	desc: "Starts the timer",
 	usage: "--start",
-}
+};
 
 commands.stop = {
 	func: util.Times.stop,
 	whitelist: times_whitelist,
 	desc: "Stops the timer",
 	usage: "--stop",
-}
+};
 
 commands.cancel = {
 	func: util.Times.cancel,
 	whitelist: times_whitelist,
 	desc: "Cancels the timer",
 	usage: "--cancel",
-}
+};
 
 commands.clean = {
 	func: util.Times.clean,
 	whitelist: times_whitelist,
-	desc: "Cleans the table, by combining rows"
+	desc: "Cleans the table, by combining rows",
 	usage: "--clean",
 	admin: true,
+};
 
 Object.freeze(commands);
 
