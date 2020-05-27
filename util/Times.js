8237b8502b49e43f61efba99349b42a1d20ae8fd
ashbob999 <ashbob999@gmail.com>
2020-05-27 15:58:08 +0100

    Moved sql queries to a sperate file.

--- util/Times.js
+++ util/Times.js
@@ -1,22 +1,16 @@
 
 const db = require("./Database.js");
+const tq = require("./TimesQuery.js");
 
 const max_time = 3600; // 1 hour
 
 function start(msg) {
 	// does not update start_time after multiple --start's
-	
-	let query = `
-INSERT INTO times (user_id,
-				   start_time)
-SELECT '${msg.author.id}', '${db.dateTime()}'
-WHERE NOT EXISTS (
-	SELECT 1
-	FROM times
-	WHERE user_id = '${msg.author.id}'
-	AND time_taken is NULL
-);
-`;
+
+	let query = tq.start.format({
+		id: msg.author.id,
+		time: db.dateTime(),
+	});
 
 	let result = db.getRows(query, (r,e) => {
 		if (!e) {
@@ -32,40 +26,11 @@ WHERE NOT EXISTS (
 function stop(msg) {
 	let end_time = db.dateTime();
 
-	let query = `
-WITH start_row AS
-(
-	SELECT id,
-		(extract(epoch
-			from to_timestamp('${end_time}', 'YYYY-MM-DD HH24-MI-SS'))
-		 - extract(epoch
-			from start_time))
-		AS time_diff
-	FROM times
-	WHERE user_id = '${msg.author.id}'
-		AND time_taken IS NULL LIMIT 1
-)
-, deleted AS
-(
-	DELETE
-	FROM times
-	WHERE id = (
-			SELECT id
-			FROM start_row
-			LIMIT 1)
-		AND (
-			SELECT time_diff
-			FROM start_row
-			LIMIT 1) > ${max_time}
-)
-
-UPDATE times
-SET time_taken = time_diff
-FROM start_row
-WHERE times.id = start_row.id
-	AND time_diff <= ${max_time}
-RETURNING time_taken
-;`;
+	let query = tq.stop.format({
+		time: end_time,
+		id: msg.author.id,
+		max_time: max_time,
+	});
 
 	let result = db.getRows(query, (r,e) => {
 		if (e) throw e;
@@ -86,43 +51,8 @@ function cancel(msg) {
 
 function clean(msg) {
 	console.log("clean");
-	
-	let query = `
-WITH summed_time AS
-(
-	SELECT user_id,
-		   sum(time_taken) AS total_time
-	FROM times
-	GROUP BY user_id
-)
-, del_id AS
-(
-	SELECT id
-	FROM (
-		SELECT id,
-		rank() OVER (
-			PARTITION BY user_id
-			ORDER BY time_taken DESC
-		)
-		FROM times
-		WHERE time_taken IS NOT NULL
-	) filtered
-	WHERE RANK > 1
-)
-, updated AS
-(
-	UPDATE times SET time_taken = st.total_time
-	FROM summed_time AS st
-	WHERE times.user_id = st.user_id
-		AND time_taken IS NOT NULL
-)
 
-DELETE
-FROM times
-WHERE id IN (
-	SELECT id
-	FROM del_id
-) ;`;
+	let query = tq.clean.format();
 
 	let result = db.getRows(query, (r,e) => {
 		if (!e) {
---
+++ util/TimesQuery.js
@@ -0,0 +1,98 @@
+
+const keywords = [
+	"id",
+	"time",
+	"max_time",
+]
+
+const start = `
+INSERT INTO times (user_id,
+				   start_time)
+SELECT '{id}', '{time}'
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM times
+	WHERE user_id = '{id}'
+	AND time_taken is NULL
+) ;
+`;
+
+const stop = `
+WITH start_row AS
+(
+	SELECT id,
+		(extract(epoch
+			from to_timestamp('{time}', 'YYYY-MM-DD HH24:MI:SS'))
+		 - extract(epoch
+			from start_time))
+		AS time_diff
+	FROM times
+	WHERE user_id = '{id}'
+		AND time_taken IS NULL LIMIT 1
+)
+, deleted AS
+(
+	DELETE
+	FROM times
+	WHERE id = (
+			SELECT id
+			FROM start_row
+			LIMIT 1)
+		AND (
+			SELECT time_diff
+			FROM start_row
+			LIMIT 1) > {max_time}
+)
+
+UPDATE times
+SET time_taken = time_diff
+FROM start_row
+WHERE times.id = start_row.id
+	AND time_diff <= {max_time}
+RETURNING time_taken ;
+`;
+
+const clean = `
+WITH summed_time AS
+(
+	SELECT user_id,
+		   sum(time_taken) AS total_time
+	FROM times
+	GROUP BY user_id
+)
+, del_id AS
+(
+	SELECT id
+	FROM (
+		SELECT id,
+		rank() OVER (
+			PARTITION BY user_id
+			ORDER BY time_taken DESC
+		)
+		FROM times
+		WHERE time_taken IS NOT NULL
+	) filtered
+	WHERE RANK > 1
+)
+, updated AS
+(
+	UPDATE times SET time_taken = st.total_time
+	FROM summed_time AS st
+	WHERE times.user_id = st.user_id
+		AND time_taken IS NOT NULL
+)
+
+DELETE
+FROM times
+WHERE id IN (
+	SELECT id
+	FROM del_id
+) ;
+`;
+
+module.exports = {
+	keywords,
+	start,
+	stop,
+	clean,
+}
