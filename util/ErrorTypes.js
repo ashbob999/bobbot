fd3c094e28cf86c24e0339752b62442e203182e6
ashbob999 <ashbob999@gmail.com>
2020-06-03 16:57:07 +0100

    Renamed error type 'NOT_ADMIN' to 'REQUIRES_ADMIN'.

--- util/ErrorTypes.js
+++ util/ErrorTypes.js
@@ -2,7 +2,7 @@
 // list of error types for a message
 // (effectively an enum)
 const errors = {
-	NOT_ADMIN: "not_admin",
+	REQUIRES_ADMIN: "requires_admin",
 	INVALID_COMMAND: "invalid_command",
 	MISSING_ARGS: "missing_args",
 };
