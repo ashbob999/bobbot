2c9dd1cee75df6b3584b51c6a6ee0aa7cb088893
ashbob999 <ashbob999@gmail.com>
2020-06-05 18:05:11 +0100

    Fixed typo in whitelisted error.

--- util/ErrorTypes.js
+++ util/ErrorTypes.js
@@ -5,7 +5,7 @@ const errors = {
 	REQUIRES_ADMIN: "ERROR_requires_admin",
 	INVALID_COMMAND: "ERROR_invalid_command",
 	MISSING_ARGS: "ERROR_missing_args",
-	NOT_WHITELISETED: "ERROR_channel_not_whitelisted",
+	NOT_WHITELISTED: "ERROR_channel_not_whitelisted",
 };
 
 // make values immutable
