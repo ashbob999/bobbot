7e8104ff6bc7041ce2e1f3296dfa9debe91856cb
ashbob999 <ashbob999@gmail.com>
2020-06-09 15:08:32 +0100

    Added simple help message for specific commands.

--- commands/Help.js
+++ commands/Help.js
@@ -2,6 +2,19 @@
 function help(bot, info) {
 	if (info.arguments.length > 1) {
 		// show help for specific command
+		if (bot.commands.has(info.arguments[1])) {
+			let command = bot.commands.get(info.arguments[1]);
+			
+			let msg = "help for '" + command.name + "' command:\n";
+			msg += command.help + "\n";
+			msg += "usage:\n";
+			msg += "\t\t" + command.usage + "\n";
+			msg += "requires admin: " + (command.admin == undefined ? false : command.admin) + "\n";
+		
+			info.message.channel.send(msg);
+		} else {
+			// invalid command name
+		}
 	} else {
 		let msg = "All Commands:\n";
 		msg += "use `help <command>` for help on a specific command.\n"
