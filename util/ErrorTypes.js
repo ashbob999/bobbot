e1823f2acd35e46d1c2fe1f735148695ab5db0c4
ashbob999 <ashbob999@gmail.com>
2020-06-16 10:55:51 +0100

    Added 'invalid sub command' error type.

--- util/ErrorTypes.js
+++ util/ErrorTypes.js
@@ -4,6 +4,7 @@
 const errors = {
 	REQUIRES_ADMIN: "ERROR_requires_admin",
 	INVALID_COMMAND: "ERROR_invalid_command",
+	INVALID_SUB_COMMAND: "ERROR_invalid_sub_command",
 	MISSING_ARGS: "ERROR_missing_args",
 	NOT_WHITELISTED: "ERROR_channel_not_whitelisted",
 };
