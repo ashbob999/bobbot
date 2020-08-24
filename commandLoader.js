46afd9249efd1351bb6e60e052a32c4b99c2835d
ashbob999 <ashbob999@gmail.com>
2020-08-24 14:04:33 +0100

    Added load sub commands

--- commandLoader.js
+++ commandLoader.js
@@ -18,7 +18,19 @@ function load(cmds) {
 }
 
 function loadSubs(cmds) {
+	// get list of all folders
+	// exclude folders starting with underscore
+	const commandFolders = fs.readdirSync("./Commands", {withFileTypes : true})
+							 .filter(dirent => dirent.isDirectory() && !dirent.name.startsWith("_"))
+							 .map(dirent => dirent.name);
 
+	for (const folder of commandFolders) {
+		const command = require(`./Commands/${folder}`);
+
+		// set a new item in the Collection
+		// with the key as the command name and the value as the exported module
+		cmds.set(command.name, command);
+	}
 }
 
 module.exports = {
