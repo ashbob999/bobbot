1d3606f60016d96d16a164426d50cd8a6b4d68de
ashbob999 <ashbob999@gmail.com>
2020-09-21 15:20:09 +0100

    Added functionality to show specified user's times.

--- Commands/times/show.js
+++ Commands/times/show.js
@@ -3,8 +3,60 @@
 const db = module.parent.db;
 const tq = module.parent.timesQuery;
 
-function show(bot, info) {
+const Discord = require("discord.js");
 
+function show(bot, info) {
+	console.log(info.arguments);
+	if (info.arguments.length == 1) {
+		// show all users times
+	} else {
+		// show given users times
+		let ids = [];
+		for (let i = 1; i < info.arguments.length; i++) {
+			let arg = info.arguments[i];
+			if (arg.startsWith("<@") && arg.endsWith(">")) {
+				arg = arg.slice(2, -1);
+				ids.push("'" + arg + "'");
+			}
+		}
+
+		if (ids.length == 0) {
+			return;
+		}
+
+		ids = "(" + ids.join(",") + ")";
+
+		let query = tq.show_users.format({
+			ids: ids,
+		});
+
+		let result = db.getRows(query, (r, e) => {
+			if (!e) {
+				if (r.rowCount > 0) {
+					const embed = new Discord.RichEmbed();
+					
+					embed.setColor("#0000ff");
+
+					embed.setTitle("**Times Taken:**");
+
+					r.rows.forEach(e => {
+						embed.addField(getNickname(e["user_id"], info.message), 
+									   db.secondsToTime(e["total_time"]));
+					});
+
+					info.message.channel.send(embed);
+				}
+			} else {
+				throw e;
+			}
+		});
+	}
+}
+
+function getNickname(id, message) {
+	let guild = message.guild;
+	let member = guild.members.get(id);
+	return member.displayName;
 }
 
 module.exports = {
