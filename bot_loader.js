0fdf64955dcbf379e77ef7a87fb9b8915fb4a7a8
ashbob999 <ashbob999@gmail.com>
2020-09-28 15:33:00 +0100

    Added run, sleep function.
    Run function starts, the loop, and checks when to switch bots every 20 minutes.
    Sleep function waits an amount of time.

--- bot_loader.js
+++ bot_loader.js
@@ -11,8 +11,64 @@ bot 1 does 00:00 -> 12:00 (am)
 bot 2 does 12:00 -> 00:00 (pm)
 */
 
-function start() {
+// sleep function
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+async function run() {
+	while (true) {
+		// check time
+		let now = new Date();
+		let bot_id = process.env.ID;
+		let hours = now.getUTCHours();
+		let minutes = now.getUTCMinutes();
+
+		// check if we should stop
+		if (bot_id == 1) {
+			if (hours == 11 && minutes >= 40) {
+				// start bot 2 (ping url)
+				console.log("ping bot 2");
+
+				return;
+			}
+		} else if (bot_id == 2) {
+			if (hours == 23 && minutes >= 40) {
+				// start bot 1 (ping url)
+				console.log("ping bot 1");
+
+				return;
+			}
+		}
+
+		// wait 20 minutes
+		await sleep(20*3600*1000);
+	}
+}
+
+function start() {
+	let now = new Date();
+	let hours = now.getUTCHours();
+	let bot_id = process.env.ID;
+	
+	// check if shoud do out-of-hours timeout
+	if (process.env.timeout == undefined) {
+		// stop if running out of hours
+		if (bot_id == 1) {
+			if (hours >= 12) {
+				console.log("bot 1, out of hours");
+				process.exit();
+			}
+		} else if (bot_id == 2) {
+			if (hours < 12) {
+				console.log("bot 2, out of hours");
+				process.exit();
+			}
+		}
+	}
+
+	// start timer
+	run();
 }
 
 module.exports = {
